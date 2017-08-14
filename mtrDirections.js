b8465de6b56cd85d45d8337df677cf553471632e
Corah Chiu <corahchiu@gmail.com>
2017-08-14 15:17:13 +0800

    update

--- mtrDirections.js
+++ mtrDirections.js
@@ -11,15 +11,15 @@ var mapsDirectionsToken = 'AIzaSyBoFN8cy4YjlKB8EF6mccM6Re4DOzzMn04'
 var origin = 'Wan Chai Station'
 var destination = 'Wong Tai Sin Station'
 
-axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + 
-'&destination=' + destination + '&mode=transit&key=AIzaSyBoFN8cy4YjlKB8EF6mccM6Re4DOzzMn04')
-    .then((response) => {
-        console.log(response.data.routes[0].legs[0].duration.text)
-        console.log(response.data.routes[0])
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+// axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin +
+// '&destination=' + destination + '&mode=transit&key=AIzaSyBoFN8cy4YjlKB8EF6mccM6Re4DOzzMn04')
+//     .then((response) => {
+//         console.log(response.data.routes[0].legs[0].duration.text)
+//         console.log(response.data.routes[0])
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
 
 // axios.get('http://www.mtr.com.hk/en/customer/services/system_map.html')
 //     .then((response)=>{
@@ -38,21 +38,21 @@ axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origi
 //         console.log(err)
 //     })
 
-// axios.get('http://www.mtr.com.hk/en/customer/services/service_hours_search.php?query_type=search&station=8')
-//     .then((response) => {
-//         let $ = cheerio.load(response.data);
-//         $.prototype.logHtml = function () {
-//             console.log(this.html());
-//         };
-//         $('.resultWrap').logHtml();
-//         var count = $('.resultWrap div:nth-child(6) table tr').children().length;
-//         console.log(count)
-//         // $('div.resultWrap div:nth-child(3) table tr:nth-child(2)').logHtml();
-//         // $('div.resultWrap div:nth-child(6) table tr:nth-child(2)').logHtml();
-//         // $('div.resultWrap div:nth-child(6) table tr:nth-child(3)').logHtml();
-//         // var first = $('tr:nth-child(2) td:nth-child(2)').text();
-//         // var last = $('tr:nth-child(2) td:nth-child(3)').text();
-//         // times.push(first)
-//         // times.push(last)
-//         // console.log(times)
-//     })
+axios.get('http://www.mtr.com.hk/en/customer/services/service_hours_search.php?query_type=search&station=8')
+    .then((response) => {
+        let $ = cheerio.load(response.data);
+        $.prototype.logHtml = function () {
+            console.log(this.html());
+        };
+        $('.resultWrap').logHtml();
+        // var count = $('.resultWrap div:nth-child(6) table tr').children().length;
+        // console.log(count)
+        $('div.resultWrap div:nth-child(3) table tr:nth-child(2)').logHtml();
+        $('div.resultWrap div:nth-child(6) table tr:nth-child(2)').logHtml();
+        // $('div.resultWrap div:nth-child(6) table tr:nth-child(3)').logHtml();
+        // var first = $('tr:nth-child(2) td:nth-child(2)').text();
+        // var last = $('tr:nth-child(2) td:nth-child(3)').text();
+        // times.push(first)
+        // times.push(last)
+        // console.log(times)
+    })
